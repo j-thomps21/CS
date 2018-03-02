4426fe81e92ee3409a97af04e61ba4fab360017f
jthompson <joshua.thompsonw@sbcglobal.net>
2018-03-01 21:04:35 -0800

    finished mytouch

--- IC221/labs/06/mytouch/mytouch.c
+++ IC221/labs/06/mytouch/mytouch.c
@@ -11,8 +11,17 @@ int main(int argc, char *argv[]){
   struct stat sb;
   struct timeval tv[2];
 
+  if(stat(argv[1], &sb) < 0)
+  {
+    perror(argv[1]);
+    return 2;
+  }
+
+  printf("Last modified: %s", ctime(&(sb.st_mtime)));
+  gettimeofday(tv, NULL);
+  utimes(argv[1], NULL);
+  printf("Last modified: %s", ctime(&(tv->tv_sec)));
   //TODO: Complete the touch operation
 
   return 0;
 }
-
---
+++ IC221/labs/06/mytouch/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  printf("%s\n", ctime(&(tv.tv_sec)));
+  return 0;
+}
