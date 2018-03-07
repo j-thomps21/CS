9c6ea9d3ae782226cafe026801c84abb30296773
jthompson <joshua.thompsonw@sbcglobal.net>
2018-03-06 20:32:14 -0800

    added some comments and changed some code

---
+++ IC211/labs/lab08/Day.java
@@ -0,0 +1,57 @@
+/**
+*/
+public class Day extends Events
+{
+  /**
+  amount variable is for "every command" ex: "every 2 days" amount = 2
+  
+  */
+  private int amount;
+  private int total = 0;
+
+  /**
+  */
+  public Day(MyDate d, String i, String t, int a)
+  {
+    super(d, i, t);
+    amount = a;
+  }
+
+  /**
+  */
+  public String toString()
+  {
+    return super.toString();
+  }
+
+  /**
+  */
+  public int getAmount()
+  {
+    return amount;
+  }
+
+  /**
+  */
+  public int getTotal()
+  {
+    return total;
+  }
+
+  /**
+  */
+  public boolean ifEventHappen()
+  {
+    total++;
+    return total % amount == 0
+  }
+
+  /**
+  */
+  public void doEvent()
+  {
+    System.out.println(toString());
+    super.getDate().incDay(amount);
+  }
+
+}
---
+++ IC211/labs/lab08/Month.java
@@ -0,0 +1,55 @@
+/**
+*/
+public class Month extends Event
+{
+  /**
+  */
+  private int amount;
+  private int total = 0;
+
+  /**
+  */
+  public Month(MyDate d, String i, String t, int a)
+  {
+    super(d, i, t);
+    amount = a;
+  }
+
+  /**
+  toString method for this class
+  */
+  public String toString()
+  {
+    return super.toString();
+  }
+
+  /**
+  */
+  public int getAmount()
+  {
+    return amount;
+  }
+
+  /**
+  */
+  public int getTotal() {
+    return total;
+  }
+
+  /**
+    */
+  public int totalInc() {
+    total++;
+  }
+
+  public boolean ifEventHappen() {
+
+
+  }
+
+  public void doEvent() {
+   System.out.println(toString());
+   getDate.incMonth(amount);
+   }
+
+}
