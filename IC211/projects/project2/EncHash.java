1feea9b3746e192299197c4e81da0cdd8547d21d
jthompson <joshua.thompsonw@sbcglobal.net>
2018-03-31 09:29:27 -0700

    part 2 complete

--- IC211/projects/project2/EncHash.java
+++ IC211/projects/project2/EncHash.java
@@ -32,6 +32,11 @@ public class EncHash implements Hasher
   */
   public void init(char[] p)
   {
+    for(int i = 0; i < p.length; i++)
+    {
+      if(p[i] > 122 || p[i] < 42)
+        throw new Throwable("error " + p[i] + " not allowed in password");
+    }
     pwd = p;
   }
 
